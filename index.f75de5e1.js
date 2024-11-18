6901cf3fb65c8b73032dd152e208181605d26b92
beliyandr <beliyandr@gmail.com>
2024-11-18 19:50:36 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,42 @@
+"use strict";
+const menu = document.querySelector(".header__menu");
+const menuLinks = menu.querySelectorAll(".menu__link");
+const menuBurger = document.querySelector(".header__burder");
+const menuClose = document.querySelector(".menu__close");
+const body = document.querySelector(".wrapper");
+const form = document.querySelector(".subscribe__form");
+function activeMenu() {
+    menu.classList.add("active");
+    body.style.overflowY = "hidden";
+    menuLinks.forEach((item)=>{
+        item.addEventListener("click", closeMenu);
+    });
+}
+function closeMenu() {
+    menu.classList.remove("active");
+    body.style.overflowY = "auto";
+}
+function submitForm() {
+    const input = form.querySelector("input");
+    input.value = "";
+}
+menuBurger.addEventListener("click", activeMenu);
+menuClose.addEventListener("click", closeMenu);
+/* --- */ $(".slider").slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    centerPadding: "20px",
+    responsive: [
+        {
+            breakpoint: 640,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
